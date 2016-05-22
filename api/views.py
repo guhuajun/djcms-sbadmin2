059ef357106d7339910eb5dbeddd1cd1525e1c96
Huajun Gu <ghjconan@hotmail.com>
2016-05-22 15:26:03 +0800

    update views, templates and styles.

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-# pylint: disable=E0401, C0111, R0903, C1001, W0232, R0901, E1101
+# pylint: disable=E0401, C0111, R0903, C1001, W0232, R0901, E1101, W0613, R0201
 
 '''
 Dashboard Panel Data Model Serializer
@@ -20,8 +20,12 @@ class DashboardPanelDataSourceViewSet(viewsets.ModelViewSet):
 
 class FlotChart1(APIView):
 
-    def get(self, request, format=None):
+    def get(self, request):
+        data = []
+        for x in xrange(0, 100):
+            data.append([x*0.1, math.sin(x*0.1)])
         response_data = {
-            "data": [(x*0.1, float(math.sin(x*0.1))) for x in xrange(-30, 31)]
+            "data": data,
+            "label": "sin(x)"
         }
         return Response(response_data)
